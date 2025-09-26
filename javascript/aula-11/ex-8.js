871b7a4e393049986eedfff06f6d5555d1189420
Nicole Lourenço <nicolelourenco38@gmail.com>
2025-09-26 18:26:05 -0300

    exercícios da aula 11

---
+++ javascript/aula-11/ex-8.js
@@ -0,0 +1,10 @@
+// criando uma variável e imprimindo o resultado, em uma sequência, usando node.js com o console.log, a tecla para ativar o node.js é F8
+let vol = 78;
+console.log('A velocidade do seu carro é de ' + vol + 'km/h');
+// criando uma condição e automatizando ela com o if, sendo uma condição simples porque não tem else
+if (vol > 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!');
+}
+
+
+console.log("Dirija sempre usando o cinto de segurança");
