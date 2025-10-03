c93e7b53d91c7b19803b39def5d8f12727999b39
Nicole Lourenço <nicolelourenco38@gmail.com>
2025-10-02 22:34:37 -0300

    Exercício 12 da aula 11

---
+++ javascript/aula-12/ex11.js
@@ -0,0 +1,8 @@
+let idade = 16
+if (idade < 16) {
+    console.log('Não pode entrar')
+} else if (idade < 18) {
+    console.log('Pode entrar com acompanhante')
+} else  {
+    console.log('Pode entrar')
+}
