3b6391f0c8500d4d49b296ab94bad20d195dd7a2
Nicole Lourenço <nicolelourenco38@gmail.com>
2025-10-04 12:00:00 -0300

    Update ex11.js

--- javascript/aula-12/ex11.js
+++ javascript/aula-12/ex11.js
@@ -1,7 +1,8 @@
-let idade = 16
+let idade = 15
+console.log(`Voce tem ${idade} anos`)
 if (idade < 16) {
     console.log('Não pode entrar')
-} else if (idade < 18) {
+} else if (idade < 18 || idade > 65) {
     console.log('Pode entrar com acompanhante')
 } else  {
     console.log('Pode entrar')
