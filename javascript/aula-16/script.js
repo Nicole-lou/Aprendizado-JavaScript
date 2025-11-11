7c0aa4191a835c4861170285062ede1c3f1625ee
Nicole Lourenço <nicolelourenco38@gmail.com>
2025-11-10 22:15:33 -0300

    Update script.js

--- javascript/aula-16/script.js
+++ javascript/aula-16/script.js
@@ -2,11 +2,19 @@ function contar() {
     let inicio = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        alert('Tudo OK!')
+      res.innerHTML = 'Contando: '
+      let i = Number(inicio.value)
+      let f = Number(fim.value)
+      let p = Number(passo.value)
+
+      for(let c = i; c<=f; c+=p) {
+        res.innerHTML += ` ${c}  \u{1F609}`
+      }
     }
 
 }
